a9672d1f99767e94139c4d78ed22a83744f68a77
AkshaySaroj <131454508+AkshaySaroj@users.noreply.github.com>
2023-04-22 21:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const container=document.querySelector(".container");
+const SignupButton=document.querySelector(".green-bg button");
+
+SignupButton.addEventListener("click",
+()=>{
+
+    container.classList.toggle("change");
+}
+)
+
